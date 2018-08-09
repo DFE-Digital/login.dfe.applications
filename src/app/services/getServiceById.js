4e2d296e82effb8343bb5ba7f1b4a55537211c45
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-08-09 10:28:50 +0100

    NSA-1669 get service by id or client id

--- src/app/services/getServiceById.js
+++ src/app/services/getServiceById.js
@@ -1,21 +1,38 @@
 const {find} = require('./data');
 const logger = require('./../../infrastructure/logger');
+const {Op} = require('sequelize');
+
+const isUUID = (value) => {
+  return value.match(/^[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}$/i) ? true : false;
+};
 
 const getServiceById = async (req, res) => {
   const serviceId = req.params.id ? req.params.id.toLowerCase() : '';
   try {
-    const service = await find({
-      id: {
-        [Op.eq]: serviceId,
-      },
-    });
-    if (!service) {
-      res.status(404).send();
+    let service;
+
+    if (isUUID(serviceId)) {
+      service = await find({
+        id: {
+          [Op.eq]: serviceId,
+        },
+      });
     }
-    res.status(200).send(service);
+
+    if (!service) {
+      service = await find({
+        clientId: {
+          [Op.eq]: serviceId,
+        },
+      });
+    }
+    if (!service) {
+      return res.status(404).send();
+    }
+    return res.status(200).send(service);
   } catch (e) {
     logger.error(e);
-    res.status(500).send(e);
+    throw e;
   }
 
 };
