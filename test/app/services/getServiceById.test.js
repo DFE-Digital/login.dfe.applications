77733a7d6d26ed8b4933e04c0ad0e2c89b316cff
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-08-15 16:20:28 +0100

    NSA-1669 getById testing

---
+++ test/app/services/getServiceById.test.js
@@ -0,0 +1,72 @@
+jest.mock('./../../../src/infrastructure/repository', () => {
+  const {mockRepository, mockServiceEntity} = require('./../../utils');
+  return mockRepository({
+    services: [
+      mockServiceEntity('svc1', 'Service One', '')
+    ],
+  });
+});
+
+jest.mock('./../../../src/infrastructure/logger', () => require('./../../utils').mockLogger());
+
+const {Op} = require('sequelize');
+const {mockRequest, mockResponse} = require('./../../utils');
+const {services} = require('./../../../src/infrastructure/repository');
+const getServicesById = require('./../../../src/app/services/getServiceById');
+const servicesData = require('./../../../src/app/services/data/index');
+
+const res = mockResponse();
+
+describe('when getting users of services', () => {
+  let req;
+
+  beforeEach(() => {
+    req = mockRequest({
+      params: {
+        id: '5360944c-6ab5-458d-be6a-a5832e3a705b',
+      }
+    });
+
+    res.mockResetAll();
+  });
+
+  it('then it should find by clientId', async () => {
+    req.params.id = 'gias';
+    await getServicesById(req, res);
+    expect(services.find).toHaveBeenCalledTimes(1);
+    expect(services.find.mock.calls[0][0]).toEqual({
+      order: [
+        ['name', 'ASC'],
+      ],
+      where: {
+        clientId: {
+          [Op.eq]: 'gias'
+        }
+      }
+    });
+  });
+
+  it('then it should find by id', async () => {
+    const id = req.params.id;
+    await getServicesById(req, res);
+    expect(services.find).toHaveBeenCalledTimes(2);
+    expect(services.find.mock.calls[0][0]).toEqual({
+      order: [
+        ['name', 'ASC'],
+      ],
+      where: {
+        id: {
+          [Op.eq]: id
+        }
+      }
+    });
+  });
+
+  it('then it should send 404 response if no service defined', async () => {
+
+    await getServicesById(req, res);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+});
