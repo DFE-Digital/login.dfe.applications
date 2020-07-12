4d3b463873575e41085a3c4d615243cab27b73b5
Sumanta <>
2020-07-12 23:41:06 +0100

    Added files for PM2 process

---
+++ pm2.js
@@ -0,0 +1,95 @@
+var pm2 = require('pm2');
+var _ = require('lodash');
+const cron = require("node-cron");
+var SlackService = require('./build/slackService');
+
+const COOLING_PERIOD = 2 * 60 * 1000;
+let slackService = new SlackService();
+
+pm2.connect(function (err) {
+
+    function scaleDown() {
+        pm2.scale('dfe-app', 2, (err, procs) => {
+            if (err) {
+                let msg = 'Error SCALING Directory Instances to 2 instances, will try again';
+                console.log(msg);
+                slackService.postMessage(msg);
+
+                setTimeout(() => {
+                    scaleDown();
+                }, COOLING_PERIOD);
+            } else {
+                let msg = 'SCALED Directory Instances to 2 instances';
+                console.log(msg);
+                slackService.postMessage(msg);
+            }
+        });
+    }
+
+    function scaleUp() {
+        pm2.scale('dfe-app', '+2', (err, procs) => {
+            if (err) {
+                let msg = 'Error SCALING Directory Instances to 4 instances. Will try again';
+                console.log(msg);
+                slackService.postMessage(msg);
+
+                setTimeout(() => {
+                    scaleUp();
+                }, COOLING_PERIOD);
+            } else {
+                let msg = 'SCALED Directory Instances to 4 instances';
+                console.log(msg);
+                slackService.postMessage(msg);
+
+                setTimeout(() => {
+                    scaleDown();
+                }, COOLING_PERIOD);
+            }
+        });
+    }
+
+    function reloadCluster() {
+        pm2.list((err, list) => {
+            let instances = _.filter(list, (x) => {
+                return x.name == 'dfe-app';
+            });
+
+            if (instances.length <= 2) {
+                scaleUp();
+            }
+        })
+    }
+
+    function monitorCluster() {
+        console.log(" ---- Staring PM2 Monitoring ---- ");
+
+        cron.schedule("0 2 * * 1,5", () => {
+            console.log(" --- executing directory instance reload  --- ");
+            reloadCluster();
+        });
+    }
+
+    if (err) {
+        console.error(err);
+        process.exit(2);
+    }
+
+    pm2.stop('dfe-app', (err, proc) => {
+    })
+
+    pm2.restart('dfe-app', (err, proc) => {
+    })
+
+    pm2.start({
+        script: 'src/index.js',   // Script to be run DFE Directory 
+        name: 'dfe-app',
+        exec_mode: 'cluster',
+        instances: 2,
+    }, (err, apps) => {
+        if (err) {
+            throw err;
+        } else {
+            monitorCluster();
+        }
+    });
+});
