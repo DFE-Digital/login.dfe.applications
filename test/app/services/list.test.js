b68fe27619a5e66af346abf5a4677cc2d88af0b9
Jared Marsh <jared.marsh@education.gov.uk>
2023-07-20 21:23:37 +0100

    Update tests to add eager loading query options

--- test/app/services/list.test.js
+++ test/app/services/list.test.js
@@ -56,6 +56,7 @@ describe('when listing services', () => {
       order: [
         ['name', 'ASC'],
       ],
+      include: ['params', 'assertions'],
       limit: 3,
       offset: 6,
     });
@@ -76,6 +77,7 @@ describe('when listing services', () => {
       order: [
         ['name', 'ASC'],
       ],
+      include: ['params', 'assertions'],
       limit: 3,
       offset: 6,
     });
