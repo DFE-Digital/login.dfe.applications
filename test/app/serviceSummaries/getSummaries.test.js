928471f5bd411480b9c80ac05cbfb399a9844fb9
Jared Marsh <jared.marsh@education.gov.uk>
2023-07-26 15:21:46 +0100

    Improved mocks for summaries tests

--- test/app/serviceSummaries/getSummaries.test.js
+++ test/app/serviceSummaries/getSummaries.test.js
@@ -2,17 +2,37 @@ const mockUtils = require('../../utils');
 
 jest.mock('./../../../src/infrastructure/repository', () => {
   const { mockRepository, mockServiceEntity } = mockUtils;
+
+  const redirects = [
+    { serviceId: 'test', redirectUrl: 'https://testing1.com' },
+    { serviceId: 'test', redirectUrl: 'https://testing2.com' },
+  ];
+  const grants = [{ serviceId: 'test', grantType: 'authorization_code' }];
+  const response = [{ serviceId: 'test', responseType: 'code' }];
+  const params = [
+    { serviceId: 'test', paramName: 'serviceId', paramValue: 'test' },
+    { serviceId: 'test', paramName: 'allowManageInvite', paramValue: 'true' },
+  ];
+  const assertions = [
+    {
+      id: '1', serviceId: 'test', typeUrn: 'LogonName', value: '__k2s-id__',
+    },
+    {
+      id: '1', serviceId: 'test', typeUrn: 'EmailAddress', value: '__user.email__',
+    },
+  ];
+
   return mockRepository({
     services: [
-      mockServiceEntity('svc1', 'Service One', ''),
-      mockServiceEntity('svc2', 'Service Two', ''),
-      mockServiceEntity('svc3', 'Service Three', ''),
-      mockServiceEntity('svc4', 'Service Four', ''),
-      mockServiceEntity('svc5', 'Service Five', ''),
-      mockServiceEntity('svc6', 'Service Six', ''),
-      mockServiceEntity('svc7', 'Service Seven', ''),
-      mockServiceEntity('svc8', 'Service Eight', ''),
-      mockServiceEntity('svc9', 'Service Nine', ''),
+      mockServiceEntity('c5a04382-c972-4ac7-b42a-755a6b41b0de', 'Service One', 'Test', redirects, [], grants, response, params, 'svc1', []),
+      mockServiceEntity('2dd5ba2c-1616-4100-9629-c11df1f2b77f', 'Service Two', 'Test', [], redirects, grants, response, params, 'svc2', []),
+      mockServiceEntity('db6071bb-ef09-4b4b-b2a7-479236c307b3', 'Service Three', 'Test', redirects, redirects, grants, response, params, 'svc3', assertions),
+      mockServiceEntity('fc035093-e1a8-4acf-95bc-2d455e83376c', 'Service Four', 'Test', redirects, [], grants, response, [], 'svc4', []),
+      mockServiceEntity('8fda7e47-102f-45f0-ad59-da2d1d17a2e9', 'Service Five', 'Test', [], redirects, grants, response, [], 'svc5', []),
+      mockServiceEntity('58efd928-23a1-42bc-915f-1083663a8c88', 'Service Six', 'Test', redirects, redirects, grants, response, [], 'svc6', assertions),
+      mockServiceEntity('740194f3-2a59-4170-9541-61a366952e30', 'Service Seven', 'Test', redirects, [], grants, response, params, 'svc7', []),
+      mockServiceEntity('a73630bc-afa0-4471-b1a3-f3e874d2c081', 'Service Eight', 'Test', [], redirects, grants, response, params, 'svc8', []),
+      mockServiceEntity('6f66c408-aeda-4c4b-92ea-3fa5e0b4a4f6', 'Service Nine', 'Test', redirects, redirects, grants, response, params, 'svc9', assertions),
     ],
   });
 });
@@ -25,10 +45,11 @@ const summaryData = require('../../../src/app/serviceSummaries/data');
 
 let req;
 const res = mockUtils.mockResponse();
-const sharedBefore = () => {
+const sharedBefore = (single) => {
   req = mockUtils.mockRequest({
     params: {
-      id: '',
+      id: single ? 'c5a04382-c972-4ac7-b42a-755a6b41b0de'
+        : 'c5a04382-c972-4ac7-b42a-755a6b41b0de,8fda7e47-102f-45f0-ad59-da2d1d17a2e9,6f66c408-aeda-4c4b-92ea-3fa5e0b4a4f6',
     },
     query: {
       fields: '',
@@ -49,7 +70,7 @@ Testing scenarios:
 - When a mix of attribute and association fields are requested, then the include field of the query contains them.
 */
 describe('When retrieving information for either one or multiple services', () => {
-  beforeEach(() => sharedBefore());
+  beforeEach(() => sharedBefore(true));
 });
 
 /*
@@ -61,7 +82,7 @@ Testing scenarios:
 - Where there is a mix of UUIDs and clientIDs, then the queryOptions 'where' contains both using OR/IN.
 */
 describe('When retrieving information for multiple services', () => {
-  beforeEach(() => sharedBefore());
+  beforeEach(() => sharedBefore(false));
 });
 
 /*
@@ -72,5 +93,5 @@ Testing scenarios:
 - When a single clientId is requested, then the queryOptions 'where' contains it with an equals.
 */
 describe('When retrieving information for one service', () => {
-  beforeEach(() => sharedBefore());
+  beforeEach(() => sharedBefore(true));
 });
