8e1fa6f5d30304f913ac54af3bf376456e64a131
Lea Hayes <lea.hayes@education.gov.uk>
2024-12-05 15:51:25 +0000

    Apply automatic fixes with 'lint:fix' command.

--- src/app/services/grants/patchToken.js
+++ src/app/services/grants/patchToken.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const { updateToken } = require("./data");
 const logger = require("../../../infrastructure/logger");
 
--- src/app/services/grants/upsertGrant.js
+++ src/app/services/grants/upsertGrant.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const { upsertGrant } = require("./data");
 const logger = require("../../../infrastructure/logger");
 
