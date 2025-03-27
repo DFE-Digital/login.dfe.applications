aa9e164873fc9761adc51cb867b20a6e14ab870f
Univer-lion-lab <98389729+Univer-lion-lab@users.noreply.github.com>
2025-03-27 10:13:19 +0000

    Docker Migration (#104)

--- src/infrastructure/config/index.js
+++ src/infrastructure/config/index.js
@@ -1,44 +1,83 @@
-
-
 const fs = require('fs');
-const Path = require('path');
+const os = require('os');
+const path = require('path');
 
-const getSettingsObject = (settings) => {
-  try {
-    return JSON.parse(settings);
-  } catch {
-    return null;
-  }
-};
+require('dotenv').config();
 
-const getSettingsFromFile = (settingsPath) => {
-  if (fs.existsSync(settingsPath)) {
-    const file = fs.readFileSync(settingsPath, 'utf8');
-    try {
-      return JSON.parse(file);
-    } catch (e) {
-      throw new Error(`Error parsing config json from ${settingsPath}: ${e.message}`);
+const config = {
+  loggerSettings: {
+    applicationName: "Applications API",
+    logLevel: "info",
+    auditDb: {
+      host: process.env.PLATFORM_GLOBAL_SERVER_NAME,
+      username: process.env.SVC_SIGNIN_ADT,
+      password: process.env.SVC_SIGNIN_ADT_PASSWORD,
+      dialect: "mssql",
+      name: process.env.PLATFORM_GLOBAL_AUDIT_DATABASE_NAME,
+      encrypt: true,
+      schema: "dbo",
+      pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
     }
-  }
-  return null
-};
-
-const fetchConfig = () => {
-  if (process.env.settings) {
-    const settings = process.env.settings;
-    let settingsObject = getSettingsObject(settings);
-    if (settingsObject !== null) {
-      return settingsObject;
+  },
+  hostingEnvironment: {
+    useDevViews: false,
+    env: process.env.LOCAL_ENV || "azure",
+    host: process.env.LOCAL_HOST || process.env.STANDALONE_APPLICATIONS_HOST_NAME,
+    port: process.env.LOCAL_PORT_APPLICATION || 443,
+    sslCert: process.env.LOCAL_SSL_CERT || "",
+    sslKey: process.env.LOCAL_SSL_KEY || "",
+    protocol: "https",
+    applicationInsights: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
+    agentKeepAlive: {
+      maxSockets: 30,
+      maxFreeSockets: 10,
+      timeout: 60000,
+      keepAliveTimeout: 30000
     }
-
-    const settingsPath = Path.resolve(settings);
-    settingsObject = getSettingsFromFile(settingsPath);
-    if (settingsObject !== null) {
-      return settingsObject;
+  },
+  auth: {
+    type: "aad",
+    identityMetadata: process.env.TENANT_URL + "/.well-known/openid-configuration",
+    clientID: process.env.AAD_SHD_APP_ID
+  },
+  database: {
+    host: process.env.PLATFORM_GLOBAL_SERVER_NAME,
+    username: process.env.SVC_SIGNIN_ORG,
+    password: process.env.SVC_SIGNIN_ORG_PASSWORD,
+    dialect: "mssql",
+    name: process.env.PLATFORM_GLOBAL_ORGANISATIONS_DATABASE_NAME,
+    encrypt: true,
+    schema: "dbo",
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     }
+  },
+  notifications: {
+    slackWebHookUrl: process.env.PLATFORM_GLOBAL_SLACK_FEED,
+    envName: process.env.ENVIRONMENT_NAME
   }
+}
 
-  throw new Error('Missing configuration');
-};
+// Persist configuration to a temporary file and then point the `settings` environment
+// variable to the path of the temporary file. The `login.dfe.dao` package can then load
+// this configuration.
+function mimicLegacySettings(config) {
+  // TODO: This can be improved by refactoring the `login.dfe.dao` package.
+  const tempDirectoryPath = fs.mkdtempSync(path.join(os.tmpdir(), 'config-'));
+  const tempConfigFilePath = path.join(tempDirectoryPath, 'config.json');
 
-module.exports = fetchConfig();
+  fs.writeFileSync(tempConfigFilePath, JSON.stringify(config), { encoding: 'utf8' });
+  process.env.settings = tempConfigFilePath;
+}
+
+mimicLegacySettings(config);
+
+module.exports = config; 
