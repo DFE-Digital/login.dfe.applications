12fa577ce3509e728329ecde4b1c2e5a050a636e
Adam Mann <Adam.Mann@education.gov.uk>
2025-07-23 16:04:04 +0100

    Tidy config for local use

--- src/infrastructure/config/index.js
+++ src/infrastructure/config/index.js
@@ -29,8 +29,8 @@ const config = {
     env: process.env.LOCAL_ENV || "azure",
     host: process.env.LOCAL_HOST || process.env.STANDALONE_APPLICATIONS_HOST_NAME,
     port: process.env.LOCAL_PORT_APPLICATION || 443,
-    sslCert: process.env.LOCAL_SSL_CERT || "",
-    sslKey: process.env.LOCAL_SSL_KEY || "",
+    sslCert: process.env.LOCAL_SSL_CERT ? process.env.LOCAL_SSL_CERT.replace(/\n/g, '\n') : "",
+    sslKey: process.env.LOCAL_SSL_KEY ? process.env.LOCAL_SSL_KEY.replace(/\n/g, '\n') : "",
     protocol: "https",
     applicationInsights: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
     agentKeepAlive: {
